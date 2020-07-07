50b94dd4e11c5f2408203b44f5727c5a0713fc04
Mark Cardamis <markncardamis@gmail.com>
2020-07-08 02:34:19 +1000

    add polling to raspberry pi

--- RaspberryInterface.py
+++ RaspberryInterface.py
@@ -42,7 +42,9 @@ def post_request(payload):
 while True:
     if Counter == 10:
         Counter = 0
-        ser.write(b"GETALL")
+        ser.write(b'GETALL')
+        ser.flush()
+        time.sleep(1)
         payload = ser.readline().decode('utf-8').rstrip()
         print(payload)
         post_request(json.loads(payload))
